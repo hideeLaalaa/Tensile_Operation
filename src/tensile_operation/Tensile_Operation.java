86dc5f358fbd9c8eb0277a7e2746db0e0b4cea57
Idris Adesina <detunj@gmail.com>
2021-06-09 17:54:43 +0100

    Application UI done

---
+++ src/tensile_operation/Tensile_Operation.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tensile_operation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author detun
+ */
+public class Tensile_Operation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+//        try {
+//            
+//            URL oracle = new URL("https://google.com/"); 
+//            URLConnection yc = oracle.openConnection(); System.err.println("establishing connection...");
+//            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+//            String inputLine;
+////            System.err.println("here: "+in.readLine());
+//            while ((inputLine = in.readLine()) !=  null) {
+//                System.out.println(inputLine);
+//               
+//            }
+//            in.close();
+//            
+//            
+//        } catch (MalformedURLException ex) {
+//            Logger.getLogger(Tensile_Operation.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(Tensile_Operation.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        
+        // TODO code application logic here
+            homePage test = new homePage();
+            test.setVisible(true);
+        
+    }
+    
+}
